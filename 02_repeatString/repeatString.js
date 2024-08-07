20f4238130d028e34df14753721e43a6e20cea6d
BlazingJune <geosullivan222@gmail.com>
2024-08-06 19:45:57 -0500

    progress on JS Exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return ('ERROR')
+    }
+    addedTogether = "";
+    for (let i = 0; i < num; i++) {
+        
+        addedTogether += string;   
+    }
+        return addedTogether;
+    }
 
-};
+;
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,30 @@
-const reverseString = function() {
+const reverseString = function(string) {
+
+    const stringArray = [];
+    const stringLength = string.length; 
+
+
+ for (let r = 1; r < stringLength; r++) {
+        let stringSlicer = string.slice(-r, -(r+1));
+        stringArray.push(stringSlicer);        
+}
+
+    for (const commas of stringArray) {
+        if (commas === ',') {
+            stringReversestring.delete continue;
+        }
+        else  {
+        let stringreverseString = stringArray.join();
+        return stringreverseString;
+    }
+    
+    }
+
+    return stringreverseString;
+
 
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
